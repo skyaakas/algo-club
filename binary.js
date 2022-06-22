9cd0f703fa3579cde64d1ebe37dad59acc8802f8
stephenlambertfs <97977357+stephenlambertfs@users.noreply.github.com>
2022-06-22 16:20:26 -0600

    added solution

---
+++ binary.js
@@ -0,0 +1,19 @@
+let arr = [2,3,4,5,6,7,8,9,10];
+function binarySearch(array, target) {
+    let start = 0;
+    let end = array.length - 1;
+    while (start <= end) {
+      // Find the middle index
+      const middle = Math.floor((start + end) / 2);
+      if (array[middle] === target) {
+        return middle;
+      } else if (array[middle] < target) {
+        return start = middle + 1;
+      } else {
+        return end = middle - 1;
+      }
+    }
+    // If we got this far, we never found the target
+    return -1;
+}
+console.log(binarySearch(arr, 5))
