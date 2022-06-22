dc0ad33b06270340d1cba41fe8b28511d0dd9681
stephenlambertfs <97977357+stephenlambertfs@users.noreply.github.com>
2022-06-22 16:21:43 -0600

    added solution

--- binary.js
+++ binary.js
@@ -1,3 +1,8 @@
+/* Create the binary search alogrithm,  given the array
+ create a function that takes in an array and target value as the paraemeters,
+ search the array and return the index of the target value
+*/
+
 let arr = [2,3,4,5,6,7,8,9,10];
 function binarySearch(array, target) {
     let start = 0;
