11355610af065313cc965889bfaed6ee846ac1df
stephenlambertfs <97977357+stephenlambertfs@users.noreply.github.com>
2022-06-22 16:14:18 -0600

    adding binary search

---
+++ binary.js
@@ -0,0 +1,5 @@
+/* Create the binary search alogrithm,  given the array
+ create a function that takes in an array and target value as the paraemeters,
+ search the array and return the index of the target value
+*/
+
--- linear.js
+++ linear.js
@@ -3,10 +3,13 @@ a function to find an element in a list, the program should output the element t
 the index of the element, and if the element is not found in the list.
 */
 const arr = [1,2,3,4,5,6,7,8,9,10];
-
 function checkForN(arr, n) {
-
+    for(let i =0; i < arr.length; i++){
+        if(n == arr[i]){
+            return `${n} is found at index ${i}`
+        }
+    }
+    return `${n} is not found in the array`
 }
-
 let number = checkForN(arr, 10);
 console.log(number);
