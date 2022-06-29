54aaad0fa3d69b94113bafca9aba22f5adfe16ad
+ <+>
2022-06-29 17:04:26 -0600

    commit tree 4a0d4c2d9be01d1b9cb8ed9471979a36947c5f1a

--- recursivefn.js
+++ recursivefn.js
@@ -5,6 +5,15 @@ the algorithm should stop after the nth iteration.
 let count = 1;
 
 function recursivefn(){
+    console.log (`${count}`);
+    if(count==10){
+        return
+    }
+    else{
+        count++
+        recursivefn();
+    }
+    
     
 }
 recursivefn();
--- sum.js
+++ sum.js
@@ -1,5 +1,13 @@
 /* Write a recursive function that takes in a number, and returns the sum of all the numbers*/
 function sum(num){
+    console.log(`num`);
+     if (i=0, i<=num , i++) {
+        num += i;
+        
+     }
+    
 
-}
+    }
+
+
 console.log(sum(2));
